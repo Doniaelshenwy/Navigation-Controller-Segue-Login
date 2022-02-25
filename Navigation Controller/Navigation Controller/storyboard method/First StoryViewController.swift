997dc4f1efaf63359faff17c31634321966ce343
Donia Elshenawy <doniaelshenawyali@gmail.com>
2022-02-25 22:30:42 +0200

    storyboard method

---
+++ Navigation Controller/Navigation Controller/storyboard method/First StoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  First StoryViewController.swift
+//  Navigation Controller
+//
+//  Created by Donia Elshenawy on 25/02/2022.
+//
+
+import UIKit
+
+class First_StoryViewController: UIViewController {
+
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+    }
+    
+
+
---
+++ Navigation Controller/Navigation Controller/storyboard method/Second Story ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  Second Story ViewController.swift
+//  Navigation Controller
+//
+//  Created by Donia Elshenawy on 25/02/2022.
+//
+
+import UIKit
+
+class Second_Story_ViewController: UIViewController {
+
+    var comingMsg = ""
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+    
+
+  
+
+}
